3fdb7abf26dfecf751fcb6201ebb1d5d87bc9b9d
Tomas Coiro <coirotomas@gmail.com>
2018-12-05 20:01:17 -0300

    Logica basica de las colas
    Cada cola ahora hace el manejo de estado de los mensajes y los consumidores dependiendo su tipo

--- lib/tp_iasc/app/broadcast_queue.ex
+++ lib/tp_iasc/app/broadcast_queue.ex
@@ -2,19 +2,36 @@ defmodule TpIasc.BroadcastQueue do
   use GenServer
 
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, %{}, opts)
+    GenServer.start_link(__MODULE__, {[], nil}, opts)
   end
 
-  def init(_opts) do
+  def init({messageQueue, consumer}) do
     IO.puts("BroadcastQueue started")
-    {:ok, %{}}
+    {:ok, {messageQueue, consumer}}
+  end
+  
+  def push(pid, message) do
+	GenServer.cast(pid, {:push, message})
+  end
+  
+  def pop(pid) do
+	GenServer.call(pid, :pop)
   end
 
-  def handle_call(_msg, _from, state) do
-    {:reply, :ok, state}
+  def handle_call(:pop, _from, {[message | messageQueue], consumer}) do
+    {:reply, message, {messageQueue, nil}}
+  end
+  
+  def handle_call(:pop, _from, {[], consumer}) do
+    {:noreply, {[], _from}}
   end
 
-  def handle_cast(_msg, state) do
-    {:noreply, state}
+  def handle_cast({:push, message}, {messageQueue, nil}) do
+    {:noreply, {messageQueue ++ [message], nil}}
+  end
+  
+  def handle_cast({:push, message}, {messageQueue, consumer}) do
+	send(consumer, message)
+    {:noreply, {messageQueue, nil}}
   end
 end
--- lib/tp_iasc/app/work_queue.ex
+++ lib/tp_iasc/app/work_queue.ex
@@ -2,19 +2,36 @@ defmodule TpIasc.WorkQueue do
   use GenServer
 
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, %{}, opts)
+    GenServer.start_link(__MODULE__, {[], []}, opts)
   end
 
-  def init(_opts) do
+  def init({messageQueue, subscriberQueue}) do
     IO.puts("WorkQueue started")
-    {:ok, %{}}
+    {:ok, {messageQueue, subscriberQueue}}
+  end
+  
+  def push(pid, message) do
+	GenServer.cast(pid, {:push, message})
+  end
+  
+  def pop(pid) do
+	GenServer.call(pid, :pop)
   end
 
-  def handle_call(_msg, _from, state) do
-    {:reply, :ok, state}
+  def handle_call(:pop, _from, {[], subscriberQueue}) do
+    {:noreply, {[], [subscriberQueue ++ _from]}}
+  end
+  
+  def handle_call(:pop, _from, {[message | messageQueue], subscriberQueue}) do
+    {:reply, message, {messageQueue, subscriberQueue}}
   end
 
-  def handle_cast(_msg, state) do
-    {:noreply, state}
+  def handle_cast({:push, message}, {messageQueue, []}) do
+    {:noreply, {messageQueue ++ [message], []}}
+  end
+  
+  def handle_cast({:push, message}, {messageQueue, [subscriber | subscriberQueue]}) do
+	send(subscriber, message)
+    {:reply, {messageQueue, subscriberQueue}}
   end
 end
