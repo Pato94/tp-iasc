7d80a811afe6eb44360f9f39abdc49c9dc4535fb
Pato <Pato9406@gmail.com>
2018-12-18 00:24:21 -0300

    Remove things

--- db/lib/db/registry.ex
+++ db/lib/db/registry.ex
@@ -7,9 +7,8 @@ defmodule Db.Registry do
   @replication_level 2
   @initial_state %{
     storages: [],
-    chunk_start_to_cluster: %{},
-    next_message_id_for_queue_id: %{}, # Next id to be consumed for queue
-    next_message_id: 1 # Handles global id number
+    chunk_to_cluster: %{}, # %{{queue_id, chunk_start} => [instance]}
+    queues_info: %{} # %{queue_id => %{first_message: id, last_message: id }}
   }
 
   def start_link(state) do
@@ -36,53 +35,49 @@ defmodule Db.Registry do
     GenServer.call(@name, {:push, id, message})
   end
 
-  def handle_call({:pop, id}, _from, state = %{next_message_id_for_queue_id: next_message_id_for_queue_id}) do
+  def handle_call({:pop, id}, _from, state) do
     Logger.debug "Registry :pop called, id: #{id}"
-    message_id = next_message_id_for_queue(id, state)
-    {:ok, cluster} = find_cluster_for_message(message_id, state) |> Enum.at(0)
+    queue_info = queue_info(id, state)
+
+    {:ok, cluster} = find_cluster_for_message(id, queue_info.first_message, state) |> Enum.at(0)
 
     message = cluster
-    |> Enum.map(fn instance -> GenServer.call(instance, {:pop, message_id}) end)
+    |> Enum.map(fn instance -> GenServer.call(instance, {:pop, "#{id}-#{queue_info.first_message}"}) end)
     |> Enum.at(0)
 
+    new_queue_info = %{queue_info | first_message: queue_info.first_message + 1}
     {
       :reply,
       message,
       %{
         state |
-        next_message_id_for_queue_id: %{
-          next_message_id_for_queue_id |
-          id => message_id + 1 # TODO: This logic is wrong
-        }
+        queues_info: Map.put(state.queues_info, id, new_queue_info)
       }
     }
   end
 
-  def handle_call({:push, id, message}, _from, state = %{next_message_id: next_message_id}) do
+  def handle_call({:push, id, message}, _from, state) do
     Logger.debug "Registry :push called, id: #{id}, message: #{message}"
-    chunk_start = div(next_message_id, @chunk_size) * @chunk_size
+    queue_info = queue_info(id, state)
+    message_id = queue_info.last_message + 1
+
+    chunk_start = div(message_id, @chunk_size) * @chunk_size
     Logger.debug "Registry :push chunk_start = #{chunk_start}"
-    cluster = find_storing_cluster(next_message_id, state)
+
+    cluster = find_storing_cluster(id, message_id, state)
 
     for instance <- cluster do
-      GenServer.call(instance, {:store, next_message_id, message})
+      GenServer.call(instance, {:store, "#{id}-#{message_id}", message})
     end
 
-    next_message_id_for_queue_id = if Map.has_key?(state.next_message_id_for_queue_id, id) do
-      state.next_message_id_for_queue_id
-    else
-      Map.put(state.next_message_id_for_queue_id, id, next_message_id)
-    end
-
-    new_chunk_to_cluster = Map.put(state.chunk_start_to_cluster, chunk_start, cluster)
+    new_queue_info = %{queue_info | last_message: message_id}
     {
       :reply,
       :ok,
       %{
         state |
-        next_message_id: next_message_id + 1,
-        chunk_start_to_cluster: new_chunk_to_cluster,
-        next_message_id_for_queue_id: next_message_id_for_queue_id
+        chunk_to_cluster: Map.put(state.chunk_to_cluster, {id, chunk_start}, cluster),
+        queues_info: Map.put(state.queues_info, id, new_queue_info)
       }
     }
   end
@@ -98,20 +93,25 @@ defmodule Db.Registry do
     {:reply, :ok, state}
   end
 
-  def next_message_id_for_queue(id, %{next_message_id_for_queue_id: map}) do
-    {:ok, id} = Map.fetch(map, id)
-    id
+  def queue_info(id, %{queues_info: map}) do
+    case Map.fetch(map, id) do
+      {:ok, info} ->
+        info
+      :error ->
+        %{first_message: 1, last_message: 0}
+    end
   end
 
-  def find_cluster_for_message(message_id, %{chunk_start_to_cluster: map}) do
+  def find_cluster_for_message(queue_id, message_id, %{chunk_to_cluster: map}) do
     Map.keys(map)
-    |> Enum.map(fn start -> {start, start..(start+@chunk_size-1)} end)
+    |> Enum.filter(fn {q_id, _} -> q_id == queue_id end)
+    |> Enum.map(fn {_, start} -> {start, start..(start+@chunk_size-1)} end)
     |> Enum.filter(fn {_start, range} -> Enum.member?(range, message_id) end)
-    |> Enum.map(fn {start, _range} -> Map.fetch(map, start) end)
+    |> Enum.map(fn {start, _range} -> Map.fetch(map, {queue_id, start}) end)
   end
 
-  def find_storing_cluster(message_id, state) do
-    actual_cluster = find_cluster_for_message(message_id, state)
+  def find_storing_cluster(queue_id, message_id, state) do
+    actual_cluster = find_cluster_for_message(queue_id, message_id, state)
     if !Enum.empty?(actual_cluster) do
       Logger.debug "Registry :find_storing_cluster. cluster found"
       {:ok, list} = actual_cluster |> Enum.at(0)
