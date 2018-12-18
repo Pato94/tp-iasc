c0561e6988b53e355295eb5178e4a7ddd5b10f26
Tomas Coiro <coirotomas@gmail.com>
2018-12-18 16:21:23 -0300

    Le saco variables innecesarias

--- api/lib/api/broadcast_queue.ex
+++ api/lib/api/broadcast_queue.ex
@@ -3,8 +3,7 @@ defmodule Api.BroadcastQueue do
   require Logger
 
   def start_link(opts) do
-    [id: id] = opts
-    GenServer.start_link(__MODULE__, %{id: id, consumers: %{}}, opts)
+    GenServer.start_link(__MODULE__, %{}, opts)
   end
 
   def init(args) do
@@ -16,21 +15,21 @@ defmodule Api.BroadcastQueue do
     {:reply, :ok, state}
   end
 
-  def handle_cast({:subscribe, pid}, state = %{id: id, consumers: consumers}) do
-    subscriberQueue = consumers[pid]
+  def handle_cast({:subscribe, pid}, state) do
+    subscriberQueue = state[pid]
     if subscriberQueue do
       GenServer.cast(subscriberQueue, {:subscribe, pid})
       {:noreply, state}
     else
       id = Api.Registry.new_worker_queue(Api.Registry)
-      newConsumers = Map.put(consumers, pid, id)
+      newState = Map.put(state, pid, id)
       GenServer.cast(id, {:subscribe, pid})
-      {:noreply, %{state | consumers: newConsumers}}
+      {:noreply, newState}
     end
   end
 
-  def handle_cast({:process, message}, state = %{id: id, consumers: consumers}) do
-    for {_, v} <- consumers do
+  def handle_cast({:process, message}, state) do
+    for {_, v} <- state do
       GenServer.cast(v, {:process, message})
     end
     {:noreply, state}
